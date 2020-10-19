f73c5c1e833f5f68d56b78e3af2559e87b81834a
cboswel1 <christianboswell86@gmail.com>
2020-10-18 18:15:36 -0600

    css / img add. adjusted schema.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,5 @@
-INSERT INTO burgers (burger_name) VALUES ('Cheese'), ('Bacon'), ('Black Bean');
+INSERT INTO burgers (burger_name) VALUES ('Royale with Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Crabby Patty');
+INSERT INTO burgers (burger_name) VALUES ('Big Kahuna Burger');
+INSERT INTO burgers (burger_name) VALUES ('Le Big Mac');
+
